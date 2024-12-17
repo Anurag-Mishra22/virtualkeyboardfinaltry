376860f4a4dcb4c328559739820cf1937a27e30e
Anurag-Mishra22 <anurag2025mishra@gmail.com>
2024-12-17 10:23:42 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
-  server: {
-    port: 3000,
-  },
+  base: '/',
+
   plugins: [react()],
 });
